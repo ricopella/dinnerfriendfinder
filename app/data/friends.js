f4778cabd2e75afa2ca90feea62284b9439defd7
Narin Sundarabhaya <nrs710@gmail.com>
2017-08-06 17:40:46 -0700

    grammer edits

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,3 @@
-/* 
-
-Example Data:
-
-{
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-}
-
-*/
-
 let friendList = [{
         "name": "David Ortiz",
         "photo": "https://connorlenahan.files.wordpress.com/2013/09/davidortiz_090413_blogcut.jpg",
@@ -51,6 +28,70 @@ let friendList = [{
             5,
             3,
             5
+        ]
+    },
+    {
+        "name": "Bruce Lee",
+        "photo": "http://www.workoutlikebrucelee.com/wp-content/uploads/bruce-lee-eating-fist-of-fury-set.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            5,
+            2,
+            3,
+            5,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Idris Elba",
+        "photo": "https://pbs.twimg.com/media/C4Pp4NLWMAAMYL5.jpg",
+        "scores": [
+            5,
+            3,
+            3,
+            5,
+            1,
+            5,
+            1,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Beyonce",
+        "photo": "https://i.genius.com/fbd59269a69ca0ca21ab00527155d55fec0d7edc?url=http%3A%2F%2Ftalkingpretty.com%2Fwp-content%2Fuploads%2F2013%2F12%2FJay-z-and-Beyonce.jpg",
+        "scores": [
+            1,
+            1,
+            2,
+            4,
+            4,
+            5,
+            3,
+            2,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Jennifer Aniston",
+        "photo": "http://i.dlisted.com/wp-content/uploads/2013/07/anistonbigmac1.jpg",
+        "scores": [
+            3,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            2,
+            2,
+            1
         ]
     }
 ]
